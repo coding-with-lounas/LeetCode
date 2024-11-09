23321c831ac8ff2354b7a6ef6db23edc6b8b7ff4
coding-with-lounas <154364639+coding-with-lounas@users.noreply.github.com>
2024-11-09 16:16:53 +0100

    Time: 59 ms (57.93%), Space: 52 MB (28.04%) - LeetHub

--- 2789-counter-ii/2789-counter-ii.js
+++ 2789-counter-ii/2789-counter-ii.js
@@ -2,27 +2,53 @@
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
  */
-var createCounter = function(init) {
-      let count = init ;
-       const increment =()=>{
-            return ++count;
-        }
-      const decrement=()=>{
-           return --count;
-       }
-       const reset =()=>{
-        count = init ;
-         return count;
-        }
+// var createCounter = function(init) {
+//       let count = init ;
+//        const increment =()=>{
+//             return ++count;
+//         }
+//       const decrement=()=>{
+//            return ount;
+//        }
+//        const reset =()=>{
+//         count = init ;
+//          return count;
+//         }
     
-    return{
-      increment : increment,
-      decrement : decrement,
-      reset: reset
+//     return{
+//       increment : increment,
+//       decrement : decrement,
+//       reset: reset
+    
+//     }
+// };
+
+class Counter {
+    constructor(init){
+        this.init=init;
+        this.count= init;
+    }
+   increment(){
+            return ++this.count;
+        }
+    decrement(){
+           return --this.count;
+       }
+       reset (){
+        this.count = this.init ;
+         return this.count;
+        }
+}
+
+var createCounter = function(init) {
+    const counter = new Counter(init);
+     return{
+        increment: () => counter.increment(), // Correct: returning a function
+        decrement: () => counter.decrement(),
+        reset: () => counter.reset()
     
     }
-};
-
+}
 /**
  * const counter = createCounter(5)
  * counter.increment(); // 6
